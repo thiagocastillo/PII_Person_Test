78cadbb907af00b7b924c470cde79c58f26dcac6
Thiago Castillo <136084200+thiagocastillo@users.noreply.github.com>
2024-09-04 10:17:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<img alt="UCU" src="https://www.ucu.edu.uy/plantillas/images/logo_ucu.svg"
+width="150"/>
+
+# Universidad Católica del Uruguay
+
+## Facultad de Ingeniería y Tecnologías
+
+### Programación II
+
+# Debug &amp; Test
+
+Código de ejemplo del tema debug &amp; test.
+
+Este ejemplo usa la clase `Person` que ya conoces de repos anteriores. La tarea consiste en agregar casos de prueba para verificar que no se pueda asignar cédulas inválidas; y que se puedan asignar cuando sean válidas.
+
+Agreguen todas las pruebas que consideren necesarias.
+
+Un método de prueba por cada caso a probar.
---
+++ src/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Ucu.Poo.Persons
+{
+    /// <summary>
+    /// El programa principal.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// El punto de entrada al programa.
+        /// </summary>
+        public static void Main()
+        {
+            IntroducePersons();
+        }
+
+        private static void IntroducePersons()
+        {
+            Person lucho = new Person("Luis Suárez", "1234567-2");
+            Person chino = new Person("Sergio Rochet", "1234567-2");
+            chino.IntroduceTo(lucho);
+            lucho.IntroduceTo(chino);
+        }
+    }
+}
